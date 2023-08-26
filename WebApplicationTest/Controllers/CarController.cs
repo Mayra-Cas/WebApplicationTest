04b1f3a20268f08553582ae9d6f9befc49916ed7
09369420 <09369420@MXPF27RECJ>
2023-08-26 17:47:56 -0600

    Add project files.

---
+++ WebApplicationTest/Controllers/CarController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Controllers
+{
+    public class CarController : Controller
+    {
+        //get obtener
+        //put mandar indorm
+        //post mandar 
+        //localoshgoy:9t9/car/index
+        // GET: Car
+        [Route("Car/Index")]
+        public ActionResult Index()
+        {
+            List<Car> carModel = new List<Car>();
+            carModel.Add(new Car()
+            {
+                id = 1,
+                description = "carro rojo",
+                name = "carro 1",
+            });
+            carModel.Add(new Car()
+            {
+                id = 2,
+                description = "carro azul",
+                name = "carro 2",
+            });
+            return View(carModel);
+        }
+        //locahost:8080/Car/SearchByName/4
+        [Route("Car/SearchByName/{carro}")]
+        public ActionResult SearchByName(string carro) {
+            List<Car> carModel = new List<Car>();
+            carModel.Add(new Car()
+            {
+                id = 1,
+                description = "carro rojo",
+                name = "carro 1",
+            });
+            carModel.Add(new Car()
+            {
+                id = 2,
+                description = "carro azul",
+                name = "carro 2",
+            });
+            //linqi ui
+            Car search = new Car();
+
+            search = 
+                carModel.Where(x => x.name.Contains(carro)).FirstOrDefault();
+            if(search == null) {
+                return RedirectToRoute("Car/Index");
+            }
+            return View(search);
+        }
+
+    }
+}
